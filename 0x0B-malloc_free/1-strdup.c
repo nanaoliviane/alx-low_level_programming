a70ef0be9eef18128c7d879c78a8c05d8a8341d0
nanaoliviane <nanaoliviane@gmail.com>
2023-09-05 01:51:40 -0700

    function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+  *_strdup - copies a character string
+  *@str: string input
+  *
+  *Return: character pointer
+  */
+int _strlen_recursion(char *str);
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *pStr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	i = _strlen_recursion(str);
+	pStr = (char *)malloc((i + 1) * sizeof(char));
+	if (pStr == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		pStr[j] = str[j];
+	}
+	return (pStr);
+}
+
+/**
+  *_strlen_recursion - returns length of string
+  *@str: pointer string
+  *
+  *Return: length of string
+  */
+
+int _strlen_recursion(char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(str + 1));
+}
